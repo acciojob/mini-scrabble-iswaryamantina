2d121ebd174c0e34b608129aa5961aea53420dba
iswaryamantina <iswarya252897@gmail.com>
2023-01-13 09:07:37 +0530

    assignment completed

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-//your code here
+var textArea = document.getElementById('evaluatedText');
+var count = document.getElementById('letterCount');
+
+textArea.addEventListener('keyup', function(){
+    let text = textArea.value;
+
+    count.innerText = text.length;
+})
+
